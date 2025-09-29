b7d559c1dc4385b43b112f2f4e048bf5b53b322f
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-29 20:20:21 +0300

    add applicationuser

--- WebBook.Infrastructure/Data/ApplicationDbContext.cs
+++ WebBook.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebBook.Infrastructure.ViewModel;
 
 namespace WebBook.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext() { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
 
     }
 }
