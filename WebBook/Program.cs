206b8d10d025aedd562076070fc4a99d9a8066e5
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-29 18:23:08 +0300

    install new idenetity package

--- WebBook/Program.cs
+++ WebBook/Program.cs
@@ -1,5 +1,4 @@
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
 using WebBook.Infrastructure.Seeds;
 using WebBook.Infrastructure.ViewModel;
 
