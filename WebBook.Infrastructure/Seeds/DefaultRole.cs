18512b1ee4025a135d70f3204ac417897d7ed306
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-29 18:23:26 +0300

    install new Identity Package

--- WebBook.Infrastructure/Seeds/DefaultRole.cs
+++ WebBook.Infrastructure/Seeds/DefaultRole.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNetCore.Identity;
 using WeebBook.Domain.Entities;
 
 namespace WebBook.Infrastructure.Seeds
