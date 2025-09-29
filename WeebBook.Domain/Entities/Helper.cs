2ae0f3d5f1eed7814d705dad6d4b74784f40a933
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-29 17:00:40 +0300

    add data of applicationuser

--- WeebBook.Domain/Entities/Helper.cs
+++ WeebBook.Domain/Entities/Helper.cs
@@ -8,5 +8,11 @@
             Admin,
             Basic
         }
+
+        //Date Default user
+        public const string Email = "superadmin@domain.com";
+        public const string Password = "SuperAdmin@123*";
+        public const string UserName = "superadmin@domain.com";
+        public const string Name = "SuperAdmin";
     }
 }
