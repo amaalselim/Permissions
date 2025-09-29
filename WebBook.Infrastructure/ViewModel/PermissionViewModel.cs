28fff870968e4f5151bf6d6302dad79ed43fbf1f
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-29 20:41:06 +0300

    create viewmodel of permisiion

---
+++ WebBook.Infrastructure/ViewModel/PermissionViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace WebBook.Infrastructure.ViewModel
+{
+    public class PermissionViewModel
+    {
+        public string RoleId { get; set; } = string.Empty;
+        public string RoleName { get; set; } = string.Empty;
+        public List<RoleClaimsViewModel> Claims { get; set; } = new List<RoleClaimsViewModel>();
+
+    }
+    public class RoleClaimsViewModel
+    {
+        public string Value { get; set; } = string.Empty;
+        public bool Selected { get; set; }
+    }
+}
+
