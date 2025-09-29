008cf29304a0536486f2d79cd4eb5dd93bf2c81e
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-30 02:07:07 +0300

    add const of Modulesname

--- WeebBook.Domain/Constants/Permissions.cs
+++ WeebBook.Domain/Constants/Permissions.cs
@@ -1,6 +1,4 @@
-﻿using static WeebBook.Domain.Entities.Helper;
-
-namespace WeebBook.Domain.Constants
+﻿namespace WeebBook.Domain.Constants
 {
     public static class Permissions
     {
@@ -22,6 +20,42 @@ namespace WeebBook.Domain.Constants
                 allpermissions.AddRange(GeneratePermissionsForModule(module.ToString()));
             }
             return allpermissions;
+        }
+
+        public static class Home
+        {
+            public const string View = "Permissions.Home.View";
+            public const string Create = "Permissions.Home.Create";
+            public const string Edit = "Permissions.Home.Edit";
+            public const string Delete = "Permissions.Home.Delete";
+        }
+        public static class Accounts
+        {
+            public const string View = "Permissions.Accounts.View";
+            public const string Create = "Permissions.Accounts.Create";
+            public const string Edit = "Permissions.Accounts.Edit";
+            public const string Delete = "Permissions.Accounts.Delete";
+        }
+        public static class Roles
+        {
+            public const string View = "Permissions.Roles.View";
+            public const string Create = "Permissions.Roles.Create";
+            public const string Edit = "Permissions.Roles.Edit";
+            public const string Delete = "Permissions.Roles.Delete";
+        }
+        public static class Registers
+        {
+            public const string View = "Permissions.Registers.View";
+            public const string Create = "Permissions.Registers.Create";
+            public const string Edit = "Permissions.Registers.Edit";
+            public const string Delete = "Permissions.Registers.Delete";
+        }
+        public static class Categories
+        {
+            public const string View = "Permissions.Categories.View";
+            public const string Create = "Permissions.Categories.Create";
+            public const string Edit = "Permissions.Categories.Edit";
+            public const string Delete = "Permissions.Categories.Delete";
         }
     }
 }
