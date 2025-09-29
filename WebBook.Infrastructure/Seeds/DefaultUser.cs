49e9a132c9a1122840d773e6edd2bfd62a01af1c
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-29 17:00:25 +0300

    create method of defaultuser

---
+++ WebBook.Infrastructure/Seeds/DefaultUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using WebBook.Infrastructure.ViewModel;
+using WeebBook.Domain.Entities;
+
+namespace WebBook.Infrastructure.Seeds
+{
+    public class DefaultUser
+    {
+        public static async Task SeedSuperAdminAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            var DefaultUser = new ApplicationUser
+            {
+                UserName = Helper.UserName,
+                Email = Helper.Email,
+                Name = Helper.Name,
+                ImageUser = "photo.jpg",
+                ActiveUser = true,
+                EmailConfirmed = true
+            };
+            var user = userManager.FindByEmailAsync(DefaultUser.Email);
+            if (user.Result == null)
+            {
+                await userManager.CreateAsync(DefaultUser, Helper.Password);
+                await userManager.AddToRoleAsync(DefaultUser.Id, Helper.Roles.SuperAdmin.ToString());
+            }
+        }
+    }
+}
