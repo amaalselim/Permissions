09baec87d7016bf11e3753eeaab4b90b09bf7734
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-29 16:33:46 +0300

    create application user view Model

---
+++ WebBook.Infrastructure/ViewModel/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebBook.Infrastructure.ViewModel
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string ImageUser { get; set; }
+        public bool ActiveUser { get; set; }
+
+    }
+}
