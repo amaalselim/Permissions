ce2b27fba8c354b47795e603b462a101c5bcbaa3
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-09-29 20:40:38 +0300

    add permission method

---
+++ WebBook/Controllers/PermissionsController.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebBook.Infrastructure.ViewModel;
+using WeebBook.Domain.Constants;
+
+namespace WebBook.Controllers
+{
+    public class PermissionsController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public PermissionsController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Permission(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+            var claims = _roleManager.GetClaimsAsync(role).Result.Select(x => x.Value).ToList();
+            var allPermissions = Permissions.PermissionsList()
+                .Select(x => new RoleClaimsViewModel
+                { Value = x }).ToList();
+            foreach (var permission in allPermissions)
+            {
+                if (claims.Any(x => x == permission.Value))
+                    permission.Selected = true;
+            }
+            return View(new PermissionViewModel
+            {
+                RoleId = roleId,
+                RoleName = role.Name,
+                Claims = allPermissions
+            });
+        }
+    }
+}
